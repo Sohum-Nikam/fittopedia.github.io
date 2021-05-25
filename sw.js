ad8940a68f578dd471c487052a531918f4976ceb
Sohum Nikam <76581363+Sohum-Nikam@users.noreply.github.com>
2021-05-25 20:33:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/master.css", "./images/logo-2.png"]);
-        })
-    );
-});
+// self.addEventListener("install", e => {
+    // e.waitUntil(
+        // caches.open("static").then(cache => {
+            // return cache.addAll(["./", "./src/master.css", "./images/logo-2.png"]);
+       // })
+     // );
+/// });
 
-self.addEventListener("fetch", e => {
-    console.log('Intercepting fetch request for: ${e.request.url}');
-});
+// self.addEventListener("fetch", e => {
+    // console.log('Intercepting fetch request for: ${e.request.url}');
+// });
 
+
+
+
+// sw.js
+self.addEventListener("fetch", event => {
+    console.log("You fetched " + event.url);
+});
 
