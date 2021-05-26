790615fa9ad6983bde3010433e6405b9285ea5e3
Sohum Nikam <76581363+Sohum-Nikam@users.noreply.github.com>
2021-05-26 10:44:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,51 @@
-// self.addEventListener("install", e => {
-    // e.waitUntil(
-        // caches.open("static").then(cache => {
-            // return cache.addAll(["./", "./src/master.css", "./images/logo-2.png"]);
-       // })
-     // );
-/// });
+// sw.js
 
-// self.addEventListener("fetch", e => {
-    // console.log('Intercepting fetch request for: ${e.request.url}');
-// });
+let cache_name = "Fittopedia"; // The string used to identify our cache
+
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(cache_name)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+            .catch(err => console.log(err))
+    );
+});
+
+
+self.addEventListener("fetch", event => {
+    if (event.request.url === "https://www.fittopedia.me/") {
+        // or whatever your app's URL is
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("/index.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
+});
+
+
+
+
+self.addEventListener("install", e => {
+     e.waitUntil(
+         caches.open("static").then(cache => {
+             return cache.addAll(["./", "./src/master.css", "./images/icons-192.png"]);
+        })
+      );
+ });
+
+ self.addEventListener("fetch", e => {
+     console.log('Intercepting fetch request for: ${e.request.url}');
+ });
 
 
 
@@ -18,3 +55,7 @@ self.addEventListener("fetch", event => {
     console.log("You fetched " + event.url);
 });
 
+// sw.js
+self.addEventListener("fetch", event => {
+    console.log("You fetched " + event.url);
+});
